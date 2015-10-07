7e5e6ad708abf9a5fa5540f226c0b944af2271a4
ajhandler <alexjhandler@gmail.com>
2015-10-07 14:50:00 -0500

    add sessions

--- leaderboard.js
+++ leaderboard.js
@@ -12,13 +12,21 @@ if(Meteor.isClient){
     },
     'numberOfPlayers': function(){
       return PlayerList.find().count();
+    },
+    'selectedClass':function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return 'selected';
+      }
     }
   });
   //EVENTS
   //events keyword that specifies the events for named template
   Template.leaderboard.events({
     'click .player': function(){
-      console.log("you clicked a  player")
+     var playerId = this._id;
+     Session.set('selectedPlayer', playerId);
     }
   });
 } // end isClient
